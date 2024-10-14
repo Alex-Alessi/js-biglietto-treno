d497881ed698ea3ee1998f6a22c12c2e00524728
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-14 15:33:20 +0200

    Code complete

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ if (!isKmDaPercorrereValid) {
 //* chiedo l'età del passeggero
 //approssimo l'età così da avere un intero
 const età = Math.floor(parseInt(prompt("Inserisci la tua età: ")));
+console.log(età);
 
 //* verifico che sia valida
 const isEtàValid = !isNaN(età) && età > 0 && età < 150;
@@ -31,17 +32,31 @@ if (!isEtàValid) {
 }
 
 //! ELABORAZIONE
-//* creo una variabile costoBiglietto e calcolo il prezzo del biglietto
-let costoBiglietto = 0.21 * kmDaPercorrere;
+if (isKmDaPercorrereValid && isEtàValid) {
+  //* creo una variabile costoBiglietto e calcolo il prezzo del biglietto
+  let costoBiglietto = 0.21 * kmDaPercorrere;
+  console.log(costoBiglietto + "€");
 
-//* SE l'età è inferiore a 18
-//*     applico lo sconto del 20% alla variabile
+  //* SE l'età è inferiore a 18
+  if (età < 18) {
+    //*     applico lo sconto del 20% alla variabile
+    costoBiglietto = costoBiglietto - (20 * costoBiglietto) / 100;
+    console.log(costoBiglietto);
+  }
 
-//* ALTRIMENTI SE l'età è superiore a 65
-//*     applico lo sconto del 40% alla variabile
+  //* ALTRIMENTI SE l'età è superiore a 65
+  else if (età > 65) {
+    //*     applico lo sconto del 40% alla variabile
+    costoBiglietto = costoBiglietto - (40 * costoBiglietto) / 100;
+    console.log(costoBiglietto);
+  }
 
-//* ALTRIMENTI
-//*     il biglietto verrà pagato interamente
-
-//! OUTPUT
-//* stampo il prezzo del biglietto
+  //* ALTRIMENTI
+  else {
+    //*     il biglietto verrà pagato interamente
+    alert("Il biglietto non è soggetto a sconti!");
+  }
+  //! OUTPUT
+  //* stampo il prezzo del biglietto
+  alert("Il prezzo del biglietto è: " + costoBiglietto.toFixed(2) + "€");
+}
