a690d30c8a83f94b59aaf5c8e46492c47dd1cd0f
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-14 15:39:55 +0200

    fixed

--- script.js
+++ script.js
@@ -22,8 +22,7 @@ if (!isKmDaPercorrereValid) {
 
 //* chiedo l'età del passeggero
 //approssimo l'età così da avere un intero
-const età = Math.floor(parseInt(prompt("Inserisci la tua età: ")));
-console.log(età);
+const età = parseInt(prompt("Inserisci la tua età: "));
 
 //* verifico che sia valida
 const isEtàValid = !isNaN(età) && età > 0 && età < 150;
@@ -35,20 +34,17 @@ if (!isEtàValid) {
 if (isKmDaPercorrereValid && isEtàValid) {
   //* creo una variabile costoBiglietto e calcolo il prezzo del biglietto
   let costoBiglietto = 0.21 * kmDaPercorrere;
-  console.log(costoBiglietto + "€");
 
   //* SE l'età è inferiore a 18
   if (età < 18) {
     //*     applico lo sconto del 20% alla variabile
     costoBiglietto = costoBiglietto - (20 * costoBiglietto) / 100;
-    console.log(costoBiglietto);
   }
 
   //* ALTRIMENTI SE l'età è superiore a 65
   else if (età > 65) {
     //*     applico lo sconto del 40% alla variabile
     costoBiglietto = costoBiglietto - (40 * costoBiglietto) / 100;
-    console.log(costoBiglietto);
   }
 
   //* ALTRIMENTI
